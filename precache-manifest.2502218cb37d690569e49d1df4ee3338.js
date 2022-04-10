1ae149c85694ff1602fbe2b744797ca5b7bd2a19
Alex Mann <dev@alexmann.org.uk>
2022-04-10 14:30:50 +0100

    Updates

---
+++ precache-manifest.2502218cb37d690569e49d1df4ee3338.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d3fa4e7a0c6c0b8e0e7f58e4ed41284",
+    "url": "/what-should-i-do/index.html"
+  },
+  {
+    "revision": "e7172b1849568f5c459f",
+    "url": "/what-should-i-do/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "f5606c4ce9d172735fb8",
+    "url": "/what-should-i-do/static/js/2.39f2b518.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/what-should-i-do/static/js/2.39f2b518.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7172b1849568f5c459f",
+    "url": "/what-should-i-do/static/js/main.1520b493.chunk.js"
+  },
+  {
+    "revision": "46b4c31694db9c49bd36",
+    "url": "/what-should-i-do/static/js/runtime-main.92d9e1c3.js"
+  }
+]);
